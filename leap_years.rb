4da3be142a833be05d4e25aebbad97e605084a79
jesslns <jesslns@yahoo.com.hk>
2019-01-02 15:52:57 +0000

    Wrote thrid test

--- leap_years.rb
+++ leap_years.rb
@@ -1,6 +1,8 @@
 def leap_years(year)
   if year % 400 == 0
     return true
+  elsif year % 100 == 0 && year % 400 != 0
+    return false
   elsif year % 4 != 0
     return false
   end
--- leap_years_spec.rb
+++ leap_years_spec.rb
@@ -6,4 +6,7 @@ describe 'Leap_years' do
   it '1970' do
     expect(leap_years(1970)).to eq false
   end
+  it '1900' do
+    expect(leap_years(1900)).to eq false
+  end
 end
